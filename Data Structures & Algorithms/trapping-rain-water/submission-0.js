bfcf1119fe9846be3fa949dff6a77355875ce10a
harpreetmatharu333 <66450205+harpreetmatharu333@users.noreply.github.com>
2026-06-13 01:11:38 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+
+        let result = 0;
+
+        let left = 0;
+        let right = height.length - 1;
+
+        let leftMax = height[0];
+        let rightMax = height[height.length - 1];
+
+        while(left < right) {
+
+            if(leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                result = result + (leftMax - height[left]); 
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                result = result + (rightMax - height[right]);
+            }
+        }
+
+        return result;
+    }
+}
